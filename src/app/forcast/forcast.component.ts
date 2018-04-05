b29f2072ff3ffca34bd432c5f1733072b1ad66d6
Andy <andres.mtz044@gmail.com>
2018-04-05 16:40:51 -0500

    Add weather icons to view

--- src/app/forcast/forcast.component.ts
+++ src/app/forcast/forcast.component.ts
@@ -12,13 +12,58 @@ import { ForcastService} from '../forcast.service';
 export class ForcastComponent implements OnInit {
 
   forcast: Forecast;
-  submited: boolean;
+  icon: string;
 
   constructor(private forcastService: ForcastService) { }
   
   ngOnInit() {}
 
   onSubmit(zipCode: string): void {
-    this.forcastService.getForcast(zipCode).subscribe(data => this.forcast = data);
+    this.forcastService.getForcast(zipCode).subscribe(data => {this.forcast = data; this.getWeatherIcon()});
+  }
+
+  getWeatherIcon(): void {
+    /*
+    Selects weather icon to display
+    */
+
+    switch(this.forcast.weather[0].icon.substring(0,2)) {
+      case "02": {
+        this.icon = "assets/img/few_clouds.png"
+        break;
+      }
+      case "03": {
+        this.icon = "assets/img/scattered_clouds.png";
+        break;
+      }
+      case "04": {
+        this.icon = "assets/img/scattered_clouds.png";
+        break;
+      }
+      case "09": {
+        this.icon = "assets/img/shower_rain.png";
+        break;
+      }
+      case "10": {
+        this.icon = "assets/img/rain.png";
+        break;
+      }
+      case "11": {
+        this.icon = "assets/img/tunderstorm.png";
+        break;
+      }
+      case "13": {
+        this.icon = "assets/img/snow.png";
+        break;
+      }
+      case "50": {
+        this.icon = "assets/img/mist.png"
+        break;
+      }
+      default: {
+        this.icon = "assets/img/clear_sky_day.png";
+        break;
+      }
+    }
   }
 }
